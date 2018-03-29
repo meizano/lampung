fa6fd6964bba4ef3e6cf57c5677db7587812b68a
Meizano A.M. Djausal <meizano@eng.unila.ac.id>
2018-03-29 23:48:00 +0700

    Menonaktifkan jQuery, menambahkan fetch untuk pengambilan data.

--- js/main.js
+++ js/main.js
@@ -1,56 +1,50 @@
 /*jslint browser: true*/
-/*global $, jQuery, alert*/
 'use strict';
 var kamus = {};
 kamus = kamusLampung;
-/*
-// Mengambil data JSON dari server
-$.ajax({
-    type: "GET",
-    url: "dispatcher/indonesia2lampung.json", // URL dari data JSON
-    dataType: "json",
-    // Jika web service tidak merespon/gagal
-    error: function (XMLHttpRequest, textStatus, errorThrown) {
-        hasilTerjemah.text("responseText: " + XMLHttpRequest.responseText +
-            ", textStatus: " + textStatus +
-            ", errorThrown: " + errorThrown);
-        hasilTerjemah.removeClass();
-        hasilTerjemah.addClass("alert alert-warning");
-    }, // error
 
-    // Jika web service merespon
-    // data yang mengandung nilai JSON akan dikembalikan
-    success: function (data) {
+var hasilTerjemah = document.getElementById('hasilTerjemah');
+
+// Mengambil data JSON dari server
+var url = './dispatcher/indonesia2lampung.json'; // URL dari data JSON
+fetch(url)
+    .then((resp) => resp.json())
+    .then(function (data) {
         kamus = data;
-        hasilTerjemah.text("Siap menterjemahkan.");
-        hasilTerjemah.removeClass();
-        hasilTerjemah.addClass("alert alert-success");
-    } // success
-}); // ajax
-*/
+        hasilTerjemah.innerHTML = 'Siap menterjemahkan';
+        hasilTerjemah.classList.remove("alert", "alert-success", "alert-warning");
+        hasilTerjemah.classList.add("alert", "alert-success");
+
+    })
+    .catch(function (error) {
+        hasilTerjemah.innerHTML = JSON.stringify(error);
+        hasilTerjemah.classList.remove("alert", "alert-success", "alert-warning");
+        hasilTerjemah.classList.add("alert", "alert-warning");
+    });
+
 function terjemah(kataAsl, bhasa, strArray) {
-    let $kataA = "lpgkata",
-        $kataT = "idkata";
+    let kataA = "lpgkata",
+        kataT = "idkata";
 
 
     // Menukar variabel sesuai dengan bahasa asal
     if (bhasa === "indonesia") {
-        $kataA = "idkata";
-        $kataT = "lpgkata";
+        kataA = "idkata";
+        kataT = "lpgkata";
     } else if (bhasa === "lampung") {
-        $kataA = "lpgkata";
-        $kataT = "idkata";
+        kataA = "lpgkata";
+        kataT = "idkata";
     }
 
     let hasils = [],
         j = 0;
 
     for (let i = 0; i < strArray.length; i++) {
-        if (strArray[i][$kataA] === kataAsl) // test / match
+        if (strArray[i][kataA] === kataAsl) // test / match
         {
             hasils[j] = new Array();
-            hasils[j][0] = strArray[i][$kataA];
-            hasils[j][1] = strArray[i][$kataT];
+            hasils[j][0] = strArray[i][kataA];
+            hasils[j][1] = strArray[i][kataT];
             hasils[j][2] = strArray[i]["lpgdialek"];
             hasils[j][3] = strArray[i]["lpgaksara"];
             j++;
@@ -63,37 +57,37 @@ function terjemah(kataAsl, bhasa, strArray) {
 }
 
 function aksarakan(kataLampung) {
-    var $aksara = "";
-    var $indeks = 0;
+    var aksara = "";
+    var indeks = 0;
 
 
     if (kataLampung.charAt(0) === "a") {
-        $aksara += kataLampung.charAt(0);
-        $indeks++;
+        aksara += kataLampung.charAt(0);
+        indeks++;
     }
 
-    while ($indeks < kataLampung.length) //
+    while (indeks < kataLampung.length) //
     {
-        if (kataLampung.charAt($indeks) === "a") //
+        if (kataLampung.charAt(indeks) === "a") //
         {
-            if (kataLampung.charAt($indeks + 1) === "i" || kataLampung.charAt($indeks + 1) == "u" || kataLampung.charAt($indeks + 1) === "a") //
+            if (kataLampung.charAt(indeks + 1) === "i" || kataLampung.charAt(indeks + 1) == "u" || kataLampung.charAt(indeks + 1) === "a") //
             {
-                $aksara += kataLampung.charAt($indeks);
+                aksara += kataLampung.charAt(indeks);
             }
 
         } else {
-            $aksara += kataLampung.charAt($indeks);
+            aksara += kataLampung.charAt(indeks);
         }
-        $indeks++;
+        indeks++;
     }
 
-    if (kataLampung.charAt(kataLampung.length - 1) === "a" || kataLampung.charAt(kataLampung.length - 1) === "u" || kataLampung.charAt(kataLampung.length - 1) === "i" || kataLampung.charAt($indeks - 1) === "e" || kataLampung.charAt($indeks - 1) === "o") {
+    if (kataLampung.charAt(kataLampung.length - 1) === "a" || kataLampung.charAt(kataLampung.length - 1) === "u" || kataLampung.charAt(kataLampung.length - 1) === "i" || kataLampung.charAt(indeks - 1) === "e" || kataLampung.charAt(indeks - 1) === "o") {
 
     } else {
-        $aksara += "x";
+        aksara += "x";
     }
 
-    return $aksara;
+    return aksara;
 }
 
 
@@ -109,7 +103,6 @@ function append(parent, el) {
 
 var kataAsal = document.getElementById('kataAsal');
 var bahasa = document.getElementById("terjemahForm").elements["bahasa"];
-//var hasilTerjemah = document.getElementById('hasilTerjemah');
 
 kataAsal.onkeyup = function () {
     //Merubah ke huruf kecil semua agar tidak ada perbedaan huruf kecil dan besar
@@ -117,7 +110,7 @@ kataAsal.onkeyup = function () {
 
     //Mengosongkan nilai dan menghilangkan style
     hasilTerjemah.innerHTML = '';
-    hasilTerjemah.classList.remove();
+    hasilTerjemah.classList.remove("alert", "alert-success", "alert-warning");
 
     //menterjemahkan
     var terjemahan = terjemah(kataAl, bahasa.value, kamus);
@@ -150,7 +143,7 @@ kataAsal.onkeyup = function () {
             append(hasilTerjemah, p);
         })
     } else if (bahasa.value === "lampung") {
-                terjemahan.map(function (dt) {
+        terjemahan.map(function (dt) {
             let p = createNode('p'),
                 span1 = createNode('span'),
                 span2 = createNode('span'),
@@ -164,7 +157,7 @@ kataAsal.onkeyup = function () {
                 sup.innerHTML = dt[2];
                 append(span2, sup);
             }
-                    span3.innerHTML = " = " + dt[1];
+            span3.innerHTML = " = " + dt[1];
             append(p, span1); // memakai fungsi append ke parameter pertama
             append(p, span2);
             append(p, span3);
