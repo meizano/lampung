ef1450a0200f9f05ebdec8c270afa75fa0242f23
meizano <meizanoam@gmail.com>
2018-10-19 16:37:17 +0700

    BUGFIX: error sw.js pada chrome

--- sw.js
+++ sw.js
@@ -1,24 +1,22 @@
-var cacheName = "KamusAksaraLampung-v1";
+var VERSION = "KamusAksaraLampung-v1";
 
 var fileCache = [
-  './',
+  // './',
   './index.html',
-  'https://cdn.polyfill.io/v2/polyfill.min.js',
   './js/jquery-1.11.3.min.js',
   './js/bootstrap.min.js',
   './js/main.js',
   './js/offline.js',
   './css/bootstrap.min.css',
   './css/style.css',
-  './fonts/aksara-Lampung-Unila-v2.ttf',
-  './fonts/'
+  './fonts/aksara-Lampung-Unila-v2.ttf'
 ];
 
 
 self.addEventListener('install', function (event) {
   console.log('SW terinstal');
   event.waitUntil(
-    caches.open(cacheName)
+    caches.open(VERSION)
     .then(function (cache) {
       // Menambahkan file ke cache
       return cache.addAll(fileCache);
@@ -31,7 +29,7 @@ self.addEventListener('activate', function (event) {
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
       return Promise.all(cacheNames.map(function (key) {
-        if (key !== cacheName) {
+        if (key !== VERSION) {
           console.log('Service Worker: menghapus cache lama', key);
           return caches.delete(key);
         }
@@ -42,14 +40,55 @@ self.addEventListener('activate', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function (res) {
-      if (res) {
-        return res;
-      } else {
-        return fetch(event.request);
+  var tryInCachesFirst = caches.open(VERSION).then(cache => {
+    return cache.match(event.request).then(response => {
+      if (!response) {
+        return handleNoCacheMatch(event);
       }
-    })
-  );
+      // Update cache record in the background
+      fetchFromNetworkAndCache(event);
+      // Reply with stale data
+      return response;
+    });
+  });
+  e.respondWith(tryInCachesFirst);
 });
+
+this.addEventListener('fetch', function(e) {
+  var tryInCachesFirst = caches.open(VERSION).then(cache => {
+    return cache.match(e.request).then(response => {
+      if (!response) {
+        return handleNoCacheMatch(e);
+      }
+      // Update cache record in the background
+      fetchFromNetworkAndCache(e);
+      // Reply with stale data
+      return response
+    });
+  });
+  e.respondWith(tryInCachesFirst);
+});
+
+function fetchFromNetworkAndCache(e) {
+  // DevTools opening will trigger these o-i-c requests, which this SW can't handle.
+  // There's probaly more going on here, but I'd rather just ignore this problem. :)
+  // https://github.com/paulirish/caltrainschedule.io/issues/49
+  if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') return;
+
+  return fetch(e.request).then(res => {
+    // foreign requests may be res.type === 'opaque' and missing a url
+    if (!res.url) return res;
+    // regardless, we don't want to cache other origin's assets
+    if (new URL(res.url).origin !== location.origin) return res;
+
+    return caches.open(VERSION).then(cache => {
+      // TODO: figure out if the content is new and therefore the page needs a reload.
+      cache.put(e.request, res.clone());
+      return res;
+    });
+  }).catch(err => console.error(e.request.url, err));
+}
+
+function handleNoCacheMatch(e) {
+  return fetchFromNetworkAndCache(e);
+}
